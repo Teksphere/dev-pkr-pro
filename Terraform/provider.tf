ff5781e6b65d2097e76e2bcda90bb0f3a53d80f6
Teksphere <160017075+Teksphere@users.noreply.github.com>
2024-09-06 10:32:17 +0100

    included modules folder

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -11,13 +11,3 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
-module "ec2_or_asg" {
-  source           = "./modules/ec2_asg"  # Adjust this path to where your module is located
-  ami_id           = "ami-0e86e20dae9224db8"       # Replace with a valid AMI ID for your region
-  instance_type    = "t2.medium"
-  key_name         = "uni-key"      # Replace with your actual key pair name
-  use_asg          = true                 # Set to false for standalone EC2
-  min_size         = 1
-  max_size         = 3
-  desired_capacity = 2
-}
